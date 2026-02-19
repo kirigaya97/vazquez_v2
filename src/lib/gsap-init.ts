6be002e8eabf866fbc149be419158ab0b7ba3716
Rodrigo Camino <desarrollo@rodrigocamino.com>
2026-02-19 11:04:45 -0300

    Scrolltrigger behaviour debug

--- src/lib/gsap-init.ts
+++ src/lib/gsap-init.ts
@@ -1,5 +1,4 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Observer } from "gsap/Observer";
-gsap.registerPlugin(ScrollTrigger, Observer);
-export { gsap, ScrollTrigger, Observer };
+gsap.registerPlugin(ScrollTrigger);
+export { gsap, ScrollTrigger };
